9ebff76eb83a009a14a7f5cfa333c9ea10014505
VanThien2004 <Thiennguyen13112004@gmail.com>
2024-04-30 22:46:40 +0700

    oke ...

--- app/src/main/java/com/example/bachhoaonline/MainActivity.java
+++ app/src/main/java/com/example/bachhoaonline/MainActivity.java
@@ -29,7 +29,7 @@ public class MainActivity extends AppCompatActivity {
     BottomNavigationView bottomNavigationViewTrangChu;
     NavigationView NagiNavigationViewTrangChu;
 
-
+//thiendeptrai
 
 
     @Override
