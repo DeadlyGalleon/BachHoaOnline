611b32f067fad16ea2b2263c8652eaadd7699b2f
VanThien2004 <Thiennguyen13112004@gmail.com>
2024-05-09 20:26:59 +0700

    okee..

--- app/src/main/java/com/example/bachhoaonline/adapter/sanphamadapter.java
+++ app/src/main/java/com/example/bachhoaonline/adapter/sanphamadapter.java
@@ -1,62 +1,62 @@
-//package com.example.bachhoaonline.adapter;
-//
-//import android.content.Context;
-//import android.view.LayoutInflater;
-//import android.view.View;
-//import android.view.ViewGroup;
-//import android.widget.ArrayAdapter;
-//import android.widget.ImageView;
-//import android.widget.TextView;
-//
-//import com.example.bachhoaonline.model.sanpham;
-//import com.example.bachhoaonline.R;
-//import com.squareup.picasso.Picasso;
-//
-//import java.util.List;
-//
-//public class sanphamadapter extends ArrayAdapter<sanpham> {
-//
-//    private Context context;
-//    private List<sanpham> sanPhamList;
-//
-//    public sanphamadapter(Context context, List<sanpham> sanPhamList) {
-//        super(context, R.layout.item_sanpham, sanPhamList);
-//        this.context = context;
-//        this.sanPhamList = sanPhamList;
-//    }
-//
-//    @Override
-//    public View getView(int position, View convertView, ViewGroup parent) {
-//        View view = convertView;
-//        ViewHolder viewHolder;
-//
-//        if (view == null) {
-//            LayoutInflater inflater = LayoutInflater.from(context);
-//            view = inflater.inflate(R.layout.item_sanpham, null);
-//            viewHolder = new ViewHolder();
-////            viewHolder.tenTextView = view.findViewById(R.id.tenspTextView);
-////            viewHolder.giaBanTextView = view.findViewById(R.id.giaBanTextView);
-////            viewHolder.loaiTextView = view.findViewById(R.id.loaiTextView);
-//            viewHolder.imageView = view.findViewById(R.id.imageView);
-//            view.setTag(viewHolder);
-//        } else {
-//            viewHolder = (ViewHolder) view.getTag();
-//        }
-//
-//        sanpham sanPham = sanPhamList.get(position);
-//
-//        viewHolder.tenTextView.setText(sanPham.getTensanpham());
-//        viewHolder.giaBanTextView.setText(String.valueOf(sanPham.getGiaban()) + " VND");
-//        viewHolder.loaiTextView.setText(sanPham.getLoai());
-//        Picasso.get().load(sanPham.getHinhanh()).into(viewHolder.imageView);
-//
-//        return view;
-//    }
-//
-//    private static class ViewHolder {
-//        TextView tenTextView;
-//        TextView giaBanTextView;
-//        TextView loaiTextView;
-//        ImageView imageView;
-//    }
-//}
+package com.example.bachhoaonline.adapter;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.example.bachhoaonline.model.sanpham;
+import com.example.bachhoaonline.R;
+import com.squareup.picasso.Picasso;
+
+import java.util.List;
+
+public class sanphamadapter extends ArrayAdapter<sanpham> {
+
+    private Context context;
+    private List<sanpham> sanPhamList;
+
+    public sanphamadapter(Context context, List<sanpham> sanPhamList) {
+        super(context, R.layout.item_sanpham, sanPhamList);
+        this.context = context;
+        this.sanPhamList = sanPhamList;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        View view = convertView;
+        ViewHolder viewHolder;
+
+        if (view == null) {
+            LayoutInflater inflater = LayoutInflater.from(context);
+            view = inflater.inflate(R.layout.item_sanpham, null);
+            viewHolder = new ViewHolder();
+            viewHolder.tenTextView = view.findViewById(R.id.tenspTextView);
+            viewHolder.giaBanTextView = view.findViewById(R.id.giaBanTextView);
+            viewHolder.loaiTextView = view.findViewById(R.id.loaiTextView);
+            viewHolder.imageView = view.findViewById(R.id.imageView);
+            view.setTag(viewHolder);
+        } else {
+            viewHolder = (ViewHolder) view.getTag();
+        }
+
+        sanpham sanPham = sanPhamList.get(position);
+
+        viewHolder.tenTextView.setText(sanPham.getTensanpham());
+        viewHolder.giaBanTextView.setText(String.valueOf(sanPham.getGiaban()) + " VND");
+        viewHolder.loaiTextView.setText(sanPham.getLoai());
+        Picasso.get().load(sanPham.getHinhanh()).into(viewHolder.imageView);
+
+        return view;
+    }
+
+    private static class ViewHolder {
+        TextView tenTextView;
+        TextView giaBanTextView;
+        TextView loaiTextView;
+        ImageView imageView;
+    }
+}
---
+++ app/src/main/java/com/example/bachhoaonline/htspact.java
@@ -0,0 +1,36 @@
+package com.example.bachhoaonline;
+
+import com.example.bachhoaonline.model.taikhoan;
+import android.content.Context;
+import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.ListView;
+import android.widget.Button;
+import android.widget.Toast;
+import android.widget.ViewFlipper;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.bachhoaonline.adapter.sanphamadapter;
+
+import com.example.bachhoaonline.retrofit.APIbachhoa;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationView;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import androidx.annotation.NonNull;
+import java.lang.Integer;
+import java.util.ArrayList;
+import java.util.List;
+public class htspact {
+}
