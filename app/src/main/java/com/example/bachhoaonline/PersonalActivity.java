1abaf079a0737d6169aecab6d9a15e0d79b20bb1
Lân <thanhlan11042004@gmail.com>
2024-05-10 13:30:05 +0700

    lân

--- app/src/main/java/com/example/bachhoaonline/PersonalActivity.java
+++ app/src/main/java/com/example/bachhoaonline/PersonalActivity.java
@@ -24,6 +24,7 @@ public class PersonalActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 Intent intent=new Intent(getApplicationContext(),DangNhapActivity.class);
+                startActivity(intent);
             }
         });
 
